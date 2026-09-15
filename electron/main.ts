162682f1c2ed0e5971312b1a1efc01063c7dfd2a
Qwantike <paul.moinereau@hotmail.fr>
2026-09-15 09:27:45 +0200

    initial commit : init project

---
+++ electron/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+
+let win: BrowserWindow | null = null
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
+
+    // En dev, charge l'URL de Vite. En prod, charge le build HTML.
+    if (process.env.VITE_DEV_SERVER_URL) {
+        win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    } else {
+        win.loadFile(path.join(process.env.DIST ?? 'dist', 'index.html'))
+    }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+        win = null
+    }
+})
